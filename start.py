3a5913db95cf5813299fcd0bd127cdd310ea1d1b
lunious <1164503275@qq.com>
2018-07-09 22:32:15 +0800

    2018/7/9 22:32

--- start.py
+++ start.py
@@ -1,4 +1,6 @@
 from scrapy import cmdline
+import time
+import os
 
 min = [
     'scrapy crawl testSpider',
@@ -8,4 +10,11 @@ min = [
     'scrapy crawl jokeSpider',
     'scrapy crawl zakerSpider',
 ]
-cmdline.execute(min[3].split())
+# cmdline.execute(min[3].split())
+
+
+# 定时任务
+while True:
+    print('开始执行>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+    os.system(min[3])
+    time.sleep(86400)  # 每隔一天运行一次 24*60*60=86400s
